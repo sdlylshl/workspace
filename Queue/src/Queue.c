de3cca58b366ccd2811c78c35c31a8779ac9ae27
sdlylshl <sdlylshl@163.com>
2015-02-09 16:51:24 +0800

    V1.4 add 单链表实现队列Queue

---
+++ Queue/src/Queue.c
@@ -0,0 +1,168 @@
+/*
+ * Queue.c
+ *
+ *  Created on: 2015年2月9日
+ *      Author: star
+ */
+
+#include"Queue.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/*构造一个空队列*/
+Queue *InitQueue()
+{
+    Queue *pqueue = (Queue *)malloc(sizeof(Queue));
+    if(pqueue!=NULL)
+    {
+        pqueue->head = NULL;
+        pqueue->tail = NULL;
+        pqueue->size = 0;
+    }
+    return pqueue;
+}
+
+/*销毁一个队列*/
+void DestroyQueue(Queue *pqueue)
+{
+    if(IsEmpty(pqueue)!=1)
+        ClearQueue(pqueue);
+    free(pqueue);
+}
+
+/*清空一个队列*/
+void ClearQueue(Queue *pqueue)
+{
+    while(IsEmpty(pqueue)!=1)
+    {
+        DeQueue(pqueue,NULL);
+    }
+
+}
+
+/*判断队列是否为空*/
+int IsEmpty( Queue *const pqueue)
+{
+    if(pqueue->head==NULL&&pqueue->tail==NULL&&pqueue->size==0)
+        return 1;
+    else
+        return 0;
+}
+
+/*返回队列大小*/
+int GetQueueSize( Queue *const pqueue)
+{
+    return pqueue->size;
+}
+
+/*返回队头元素*/
+Node * GetQueueHead( Queue *const pqueue,Item *pitem)
+{
+    if(IsEmpty(pqueue)!=1&&pitem!=NULL)
+    {
+        *pitem = pqueue->head->data;
+    }
+    return pqueue->head;
+}
+
+/*返回队尾元素*/
+
+Node * GetQueueTail( Queue *const pqueue,Item *pitem)
+{
+    if(IsEmpty(pqueue)!=1&&pitem!=NULL)
+    {
+        *pitem = pqueue->tail->data;
+    }
+    return pqueue->tail;
+}
+
+/*将新元素入队*/
+Node * EnQueue(Queue *pqueue,Item item)
+{
+	int a =sizeof(Node);
+	Node * pnode = (Node *)malloc(sizeof(Node));
+    if(pnode != NULL)
+    {
+        pnode->data = item;
+        pnode->next = NULL;
+
+        if(IsEmpty(pqueue))
+        {
+            pqueue->head = pnode;
+        }
+        else
+        {
+            pqueue->tail->next = pnode;
+        }
+        pqueue->tail = pnode;
+        pqueue->size++;
+    }
+    return pnode;
+}
+
+/*队头元素出队*/
+Node * DeQueue(Queue *pqueue,Item *pitem)
+{
+    Node * pnode = pqueue->head;
+    if(IsEmpty(pqueue)!=1&&pnode!=NULL)
+    {
+        if(pitem!=NULL)
+            *pitem = pnode->data;
+        pqueue->size--;
+        pqueue->head = pnode->next;
+        free(pnode);
+        if(pqueue->size==0)
+            pqueue->tail = NULL;
+    }
+    return pqueue->head;
+}
+
+/*遍历队列并对各数据项调用visit函数*/
+void QueueTraverse(Queue *pqueue,void (*operate)())
+{
+    Node * pnode = pqueue->head;
+    int i = pqueue->size;
+    while(i--)
+    {
+    	operate(pnode->data);
+        pnode = pnode->next;
+    }
+
+}
+
+
+void print(Item i)
+{
+    printf("该节点元素为%d\n",i);
+}
+void main()
+{
+    Queue *pq = InitQueue();
+    int i;
+    Item items ={
+    		10
+    };
+    printf("0-9依次入队并输出如下：\n");
+    for(i=0;i<10;i++)
+    {
+        EnQueue(pq,items);
+        GetQueueTail(pq,&items);
+        printf("%d ",items);
+    }
+
+    printf("\n从队头到队尾遍历并对每个元素执行print函数：\n");
+    QueueTraverse(pq,print);
+
+    printf("队列中元素依次出队列并输出如下：\n");
+    for(i=0;i<10;i++)
+    {
+        DeQueue(pq,&items);
+        printf("%d ",items);
+    }
+    ClearQueue(pq);
+    if(IsEmpty(pq))
+        printf("\n将队列置空成功\n");
+    DestroyQueue(pq);
+    printf("队列已被销毁\n");
+}
+
---
+++ Queue/src/Queue.h
@@ -0,0 +1,68 @@
+/*
+ * Queue.h
+ *
+ *  Created on: 2015年2月9日
+ *      Author: star
+ */
+
+
+#ifndef Queue_H
+#define Queue_H
+
+//单链表实现的队列
+typedef unsigned char * Item;
+//typedef struct _data{
+//
+//	unsigned char id[4];
+//	unsigned char checksum;
+//}Item;
+//typedef struct node * PNode;
+//定义节点数据
+//队列节点包含队列指针 和 数据成员
+typedef struct node
+{
+    Item data;
+    struct node * next;
+}Node;
+//队列由队列节点组成
+typedef struct
+{
+	Node * head;
+	Node * tail;
+    int size;
+}Queue;
+
+//1.创建一个队列并初始化为空 Queue RecvQueue={0};
+//2.创建一个节点 Node RecvNode[100];
+//3.将节点添加进队列
+/*构造一个空队列*/
+Queue *InitQueue();
+
+/*销毁一个队列*/
+void DestroyQueue(Queue *pqueue);
+
+/*清空一个队列*/
+void ClearQueue(Queue *pqueue);
+
+/*判断队列是否为空*/
+int IsEmpty(Queue * const pqueue);
+
+/*返回队列大小*/
+int GetQueueSize(Queue * const pqueue);
+
+/*返回队头元素*/
+Node * GetQueueHead(Queue * const pqueue,Item *pitem);
+
+/*返回队尾元素*/
+Node * GetQueueTail(Queue * const pqueue,Item *pitem);
+
+/*将新元素入队尾*/
+Node * EnQueue(Queue *pqueue,Item item);
+
+/*队头元素出队*/
+Node * DeQueue(Queue *pqueue,Item *pitem);
+
+/*遍历队列并对各数据项调用visit函数*/
+void QueueTraverse(Queue *pqueue,void (*operate)());
+
+#endif
